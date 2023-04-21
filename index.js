1d6779deb892a11e4cc687e294150181a0e4f202
Tayler Schanks <taylerschanks2@gmail.com>
2023-04-21 11:38:04 -0500

    lab objective

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
+function reverse(word) {
+  let wordArray = word.split("");
+  let reversedWordArray = wordArray.reverse()
+  let reversedWord = reversedWordArray.join("")
+  return reversedWord
+}
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word)
+  if (word === reversedWord) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  make a function that returns true if a word is a palindrome and false if it's not.
+  that means if the word is the same as the word in reverse, I should return true.
+
+
+  mom === mom // true
+  abc === cba // false
+
+
+  reverse the string
+  check if the input is the same as the reverse input
+    return true
+   else
+    return false
+
+ function isPalindrome(word) {
+  const reversedWord = reverse(word)
+  if (word === reversedWord) {
+    return true
+  } else {
+  return false
+  }
+}
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
